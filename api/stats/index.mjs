fdaad3ce7f7e96f0c332f6455229ff8ecf924def
zcabkle <73954803+zcabkle@users.noreply.github.com>
2023-03-02 15:36:19 +0000

    stats endpoint works

--- api/stats/index.mjs
+++ api/stats/index.mjs
@@ -4,40 +4,35 @@ import getToken from "../auth.js";
 import makeRequest from "../makeRequest.js";
 
 export default async function (context, req) {
-    const app = express();
-    app.use(cors());
+    var url = `https://org6e7090ee.api.crm4.dynamics.com/api/data/v9.2/cr967_items/$count`
 
-    app.get('/stats', async (req, res) => {
-        console.log("HERE")
-        var url = `https://org6e7090ee.api.crm4.dynamics.com/api/data/v9.2/cr967_items/$count`
+    var bearerToken = await getToken();
 
-        var bearerToken = await getToken();
+    var response = await makeRequest(bearerToken, url);
+    var json_content = await response.json();
 
-        var response = await makeRequest(bearerToken, url);
-        var json_content = await response.json();
+    var url = `https://org6e7090ee.api.crm4.dynamics.com/api/data/v9.2/cr967_foodbanks/$count`
 
-        var url = `https://org6e7090ee.api.crm4.dynamics.com/api/data/v9.2/cr967_foodbanks/$count`
+    var bearerToken = await getToken();
 
-        var bearerToken = await getToken();
+    var response = await makeRequest(bearerToken, url);
+    var json_content2 = await response.json();
 
-        var response = await makeRequest(bearerToken, url);
-        var json_content2 = await response.json();
+    var url = `https://org6e7090ee.api.crm4.dynamics.com/api/data/v9.2/cr967_outgoingrecords/$count`
 
-        var url = `https://org6e7090ee.api.crm4.dynamics.com/api/data/v9.2/cr967_outgoingrecords/$count`
+    var bearerToken = await getToken();
 
-        var bearerToken = await getToken();
+    var response = await makeRequest(bearerToken, url);
+    var json_content3 = await response.json();
 
-        var response = await makeRequest(bearerToken, url);
-        var json_content3 = await response.json();
+    const responseJSON = {
+        "items_count": json_content,
+        "foodbanks_count": json_content2, 
+        "visits_count": json_content3
+    }
 
-        const responseJSON = {
-            "items_count": json_content,
-            "foodbanks_count": json_content2, 
-            "visits_count": json_content3
-        }
+    context.res = {
+        body : responseJSON
+    };
 
-        context.res = {
-            body : responseJSON
-        };
-    })
 }

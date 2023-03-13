eea096c9cade6afc08bc3584a29958d6c510713d
zcabkle <73954803+zcabkle@users.noreply.github.com>
2023-03-13 12:03:06 +0000

    fix empty link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -97,7 +97,7 @@ function Navbar() {
             variant="h5"
             noWrap
             component="a"
-            href=""
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },

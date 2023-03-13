c37b367acf32076a584320e68bfadfe2ca0ee55a
zcabkle <73954803+zcabkle@users.noreply.github.com>
2023-03-13 11:47:28 +0000

    Tidy up routing

---
+++ src/components/Error/ErrorComponent.js
@@ -0,0 +1,5 @@
+function ErrorComponent(){
+  return <div>404 Error. Page not found. Error occured.</div>
+}
+
+export default ErrorComponent;
--- src/routing/ApplicationRouter.js
+++ src/routing/ApplicationRouter.js
@@ -3,10 +3,12 @@ import LandingPage from "../pages/landing-page/landing-page";
 import ItemsPage from "../pages/items-page/items-page.js";
 import FoodbanksPage from "../pages/foodbanks-page/foodbanks-page";
 import FoodbankItemsPage from "../pages/foodbank-items-page/foodbank-items-page";
+import AboutPage from "../pages/about-page/about-page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FoodbankParcelsPage from "../pages/foodbank-parcels-page/foodbank-parcels-page";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import ErrorComponent from "../components/Error/ErrorComponent";
 
 function ApplicationRouter() {
     return (
@@ -19,6 +21,8 @@ function ApplicationRouter() {
                     <Route path='items' element={< ItemsPage />} />
                     <Route path="items/:id" element={<FoodbankItemsPage />} />
                     <Route path="parcels/:id" element={<FoodbankParcelsPage />} />
+                    <Route path='about' element={< AboutPage />} />
+                    <Route path="*" element={<ErrorComponent/>} />
                 </Routes>
                 <Footer/>
             </Router>
